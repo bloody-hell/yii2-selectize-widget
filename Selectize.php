9cd5e571cde907eb916fbf9efb049b000850513a
Ivan Poponin <juf.halli@gmail.com>
2014-03-13 15:57:58 +0400

    fix removed redundant encoding

--- Selectize.php
+++ Selectize.php
@@ -84,7 +84,7 @@ class Selectize extends InputWidget
 			$this->clientOptions['load'] = new JsExpression("
 function (query, callback) {
 	if (!query.length) return callback();
-	$.getJSON('$url', { query: encodeURIComponent(query) }, function (data) { callback(data); })
+	$.getJSON('$url', { query: query }, function (data) { callback(data); })
 	.fail(function () { callback();	});
 }
 ");
